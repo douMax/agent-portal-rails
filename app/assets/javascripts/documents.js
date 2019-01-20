496e72fdd3c01700e401e9b18e4dbdfee9b3de56
Lingxiao Wang <xwangl213@gmail.com>
2019-01-20 22:34:36 +1100

    added upload and add-link on new document

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-  var fileInput = document.getElementById('document-upload');
+  var fileInput = document.getElementById('document-upload')
   if (fileInput) {
-    var docuTitleField = document.getElementById('document_title');
+    var docuTitleField = document.getElementById('document_title')
+    var msg = document.getElementById('upload-msg')
     fileInput.addEventListener('change', function(e){
-      var fileName = e.srcElement.files[0].name;
-      docuTitleField.value = fileName;
+    msg.textContent = ''
+      var file = e.srcElement.files[0]
+      docuTitleField.value = file.name
+
+      if (file.size > 10485760) {
+        msg.textContent = "File is too big, please use Dropbox/Google Drive links instead"
+      }
+      
     })
   }
 
 
 
-}); // docu ready
+}) // docu ready
 
